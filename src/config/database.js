0369c333ac8d4f6744436c91a484b153b6826ed7
Dubashit <andreejdubinchuk@gmail.com>
2024-10-14 15:53:33 +0300

    vercel

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,17 @@
 const { Sequelize } = require('sequelize')
 
+// const sequelize = new Sequelize({
+//     dialect: "postgres",
+//     host: "localhost",
+//     port: "5432",
+//     username: "postgres",
+//     password: "root",
+//     database: "admin"
+// })
+
 const sequelize = new Sequelize({
     dialect: "postgres",
-    host: "localhost",
-    port: "5432",
+    connectionString: process.env.POSTGRES_URL,
     username: "postgres",
     password: "root",
     database: "admin"
